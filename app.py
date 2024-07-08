de999a1cd672c7e61c73804fde9ffa3ca14919af
Lovely-logesh <146011778+Lovely-logesh@users.noreply.github.com>
2024-07-07 23:51:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 #!pip install streamlit -q
 #importing streamlit
 import streamlit as st
-from streamlit import SessionState
+#from streamlit import SessionState
 
 #image or logo
 from PIL import Image
